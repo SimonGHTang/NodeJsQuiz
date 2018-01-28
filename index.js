da561dffd08088c8b32268a88ce9efcdb8dcb2bb
Guo Han Tang <simon.gh.tang@gmail.com>
2018-01-28 21:00:48 +1100

    questions now available to view

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const models = require("./models");
 const scripts = require ("./scripts/start_script.js");
 
 const HomeRouter = require("./routes/home.server.router");
-//const StellarisSurveyRouter = require("./routes/stellaris.survey.router");
+const StellarisSurveyRouter = require("./routes/stellaris.survey.router");
 //const EditStellarisSurveyRouter = require("./routes/stellaris.edit.survey.router");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded( {extended:true} ));
 //simplifies incomeing code
 
 app.set("views", path.join(__dirname, "/views"));
@@ -26,10 +26,10 @@ app.set("view engine", "html");
 app.engine("html", require("ejs").renderFile);
 
 app.use("/", HomeRouter);
-//app.use("/StellarisSurvey", StellarisSurveyRouter, express.static(path.join(__dirname, "/public")));
-//app.use("/EditStellarisSurvey", EditStellarisSurveyRouter, express.static(path.join(__dirname, "public")))
+app.use("/StellarisSurvey", StellarisSurveyRouter, express.static(path.join(__dirname, "/public")));
+//app.use("/EditStellarisSurvey", EditStellarisSurveyRouter, express.static(path.join(__dirname, "/public")))
 
-//suncs models, drop databases, connect to database and start application
+//syncs models, drop databases, connect to database and start application
 models.sequelizeCredentials.sync({force: true}).then(() => {
     scripts.startScript();
 
---
+++ routes/stellaris.survey.router.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const models = require("../models");
+
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    models.stellaris_question.get(id).then((returnQuestion) => {
+        if(returnQuestion === null || returnQuestion === undefined) {
+            res.render("error_page.ejs", {error: "Survey question not found for Stellaris Ethnics Survey"} );
+        } else {
+            models.stellaris_question.countRows().then((returnRowCount) => {
+                rowCount = returnRowCount;
+                res.render("stellaris_question.ejs", { result: returnQuestion, surveyInfo: {id: id, rowCount:returnRowCount} });
+            });
+        }
+    }).catch(e => {
+        res.render("error_page.ejs", {error: "An unknown error occured."});
+    });
+
+});
+
+module.exports = router;
